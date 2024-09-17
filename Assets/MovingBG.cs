94d6ac50c47dd194b5285a4dd9772ef7fe5e9821
Xx-DELWYN-xX <delwynfernandes.sdcce@vvm.edu.in>
2024-09-18 05:16:49 +0530

    Commit test successful(Del)

---
+++ Assets/MovingBG.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBG : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer;
+    private Vector2 savedOffset;
+
+    void Start () {
+        renderer = GetComponent<Renderer> ();
+    }
+
+    void Update () {
+	float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+	Vector2 offset = new Vector2 (x, 0);
+	renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
+
+
+// test commit file
