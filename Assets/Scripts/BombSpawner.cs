8731e611b98f03fe0f0956df6229f34a50e52e96
mahekshaikh123 <mahekshaikh427@gmail.com>
2024-10-08 12:22:23 +0530

    commiting level2

--- Assets/Scripts/BombSpawner.cs
+++ Assets/Scripts/BombSpawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BombSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject bombPrefab;
+    public float spawnRate = 2.0f;
+    public Camera mainCamera;
+
     void Start()
     {
-        
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+        InvokeRepeating("SpawnBomb", 0f, spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnBomb()
     {
-        
+        if (bombPrefab != null)
+        {
+            Vector2 spawnPosition = GetSpawnPositionAtTop();
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    Vector2 GetSpawnPositionAtTop()
+    {
+        // Get the camera's viewport bounds
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+
+        // Spawn position should be at the top edge of the camera view
+        float spawnX = Random.Range(-cameraWidth / 2, cameraWidth / 2);
+        float spawnY = mainCamera.orthographicSize + 1; // Adjust as needed to position bombs slightly above the top edge
+
+        return new Vector2(spawnX, spawnY);
     }
 }
